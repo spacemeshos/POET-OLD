0dbb61784dada362dd1b0f2bfc68d611eedfd631
Aviv Eyal <aviveyal07@gmail.com>
2018-12-15 13:25:00 +0200

    Added BenchmarkSha256() for the contest

---
+++ server/go/poet/hash_test.go
@@ -0,0 +1,32 @@
+package poet
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/minio/sha256-simd"
+	"math"
+	"testing"
+	"time"
+)
+
+func BenchmarkSha256(t *testing.B) {
+
+	buff := bytes.Buffer{}
+	buff.Write([]byte("Seed data goes here"))
+	out := [32]byte{}
+	n := uint64(math.Pow(10, 8))
+
+	fmt.Printf("Computing %d serial sha-256...\n", n)
+
+	t1 := time.Now().Unix()
+
+	for i := uint64(0); i < n; i++ {
+		out = sha256.Sum256(buff.Bytes())
+		buff.Reset()
+		buff.Write(out[:])
+	}
+
+	d := time.Now().Unix() - t1
+	r := n / uint64(d)
+	fmt.Printf("Final hash: %x. Running time: %d secs. Hash-rate:%d hashes-per-sec\n", buff.Bytes(), d, r)
+}
